b71ba3c1482cb164417abb551e1b4705b2f9a255
Iwasaki-2310 <iwachan2310@gmail.com>
2024-09-23 14:31:05 +0900

    metaタグ

--- resources/js/Layouts/Layouts.jsx
+++ resources/js/Layouts/Layouts.jsx
@@ -1,5 +1,6 @@
 import NavBar from '@/Components/NavBar';
 import { Heading } from '@chakra-ui/react';
+import { Head } from '@inertiajs/react';
 import { useEffect } from 'react';
 
 const Layouts = ({ children }) => {
@@ -11,6 +12,11 @@ const Layouts = ({ children }) => {
   }, []);
   return (
     <>
+      <Head>
+        <title>My App</title>
+        <meta name="description" content="My Appアプリケーション" />
+        <meta name="keywords" content="My App" />
+      </Head>
       <header>
         <Heading as="h1" style={{ textAlign: 'center' }}>
           Logo
@@ -18,6 +24,7 @@ const Layouts = ({ children }) => {
         <NavBar />
       </header>
       <main>{children}</main>
+      <p>aaaa</p>
     </>
   );
 };
--- resources/js/Pages/About.jsx
+++ resources/js/Pages/About.jsx
@@ -1,10 +1,16 @@
 import Layouts from '@/Layouts/Layouts';
 import { Heading, Text } from '@chakra-ui/react';
+import { Head } from '@inertiajs/react';
 
 const About = props => {
   const { greeting } = props;
   return (
     <>
+      <Head>
+        <title>Aboutページ</title>
+        <meta name="description" content="これはAboutページです" />
+        <meta name="keywords" content="About" />
+      </Head>
       <Heading as="h2">About</Heading>
       <Text>{greeting}</Text>
     </>
