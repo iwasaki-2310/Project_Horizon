4eb460874c877d2ae871b8a608c1b7478b77087d
fit-style-iwasaki <s.iwasaki.fitstyle@gmail.com>
2024-12-25 18:03:55 +0900

    ポート回り改善

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,25 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    server: {
+        port: 5174, // コンテナ内のポート
+        host: '0.0.0.0', // 外部アクセスを許可
+        strictPort: true, // ポートが使用中の場合にエラーを出す
+        hmr: {
+            host: 'localhost', // ホストマシンのホスト名
+            port: 7777, // ホストマシンのポート
+        },
+    },
     plugins: [
         laravel({
-            input: 'resources/js/app.jsx',
+            input: ['resources/js/app.jsx', 'resources/css/app.css'],
             refresh: true,
         }),
         react(),
     ],
+    resolve: {
+        alias: {
+            '@': '/resources/js',
+        },
+    },
 });
